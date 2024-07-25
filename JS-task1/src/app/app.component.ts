4f8a2a87ad17a00627ebde14932981138d9219db
Shane Friedman <sfrie53@wgu.edu>
2024-07-25 07:17:27 -0500

    HTML/CSS template done

--- JS-task1/src/app/app.component.ts
+++ JS-task1/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { WorldComponent } from './world/world.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WorldComponent],
   templateUrl: './app.component.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
