818457f201baa2935e64c2e68a67c8b099c97ea1
Shane Friedman <sfrie53@wgu.edu>
2024-07-26 09:40:28 -0500

    Fixed routing with ngModule.

--- JS-task1/src/app/app.routes.ts
+++ JS-task1/src/app/app.routes.ts
@@ -1,9 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { WorldComponent } from './world/world.component';
 
 export const routes: Routes = [
 
-    { path: '',
-      title: 'Home map',  
+    { path: '',  
       component: WorldComponent, }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
