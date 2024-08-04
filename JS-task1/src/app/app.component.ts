518473f4dfb48352fe01981059934716a03617ff
Shane Friedman <sfrie53@wgu.edu>
2024-08-03 23:09:21 -0500

    Event handlers and variable declaration

--- JS-task1/src/app/app.component.ts
+++ JS-task1/src/app/app.component.ts
@@ -1,15 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { SvgMapComponent } from './svg-map/svg-map.component';
+import { WorldDataService } from './world-data.service';
+import { HttpClient } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    SvgMapComponent,
+    HttpClient,
+  ],
   templateUrl: './app.component.html',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'JS-task1';
+  
+  private worldDataService = inject(WorldDataService);
+  
+  //Declared variable that will hold country info
+  selectedSvgId: string = '';
+  selectedIncome: string = '';
+  selectedCapital: string = '';
+  selectedLongitude: string = '';
+  selectedLatitude: string = '';
+  selectedRegion: string = '';
+
+  //Event Handlers that will be called when events from click events occur
+  onSvgIdSelected(svgId: string) {
+    this.selectedSvgId = svgId;
+  }
+
+  onIncomeSelected(incomeId: string) {
+    this.selectedIncome = incomeId;
+  }
+  
+  onCapitalSelected(capital: string) {
+    this.selectedCapital = capital;
+  }
+
+  onLongitudeSelected(longitude: string) {
+    this.selectedLongitude = longitude;
+  }
+
+  onLatitudeSelected(latitude: string) {
+    this.selectedLatitude = latitude;
+  }
+
+  onRegionSelected(region: string) {
+    this.selectedRegion = region;
+  }
 }
