d8798579f61504b0cbfd26e8121e6bf8c25093cb
Shane Friedman <sfrie53@wgu.edu>
2024-08-03 21:43:17 -0500

    Api connection is initialized

--- JS-task1/src/app/world-data.service.ts
+++ JS-task1/src/app/world-data.service.ts
@@ -5,19 +5,18 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' //Service is available globally
 })
 export class WorldDataService {
-  
+  private apiUrl = 'https://api.worldbank.org/v2/country/'; // Base URL
 
-  constructor(private http: HttpClient) { }
-
-  
+  constructor(private http: HttpClient) { 
+    console.log('WorldDataService is initialized'); //Logs to console if api connects
+  }
 
 getCountryInformation(countryId: string): Observable<any> {
-  const apiUrl = `http://api.worldbank.org/v2/country/${countryId}?format=json`;
-  return this.http.get(apiUrl);
+  return this.http.get<any>(`${this.apiUrl}${countryId}?format=json`);
   
-}
+  }
 }
 
